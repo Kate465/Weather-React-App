28ad89f3cc02996ceeded4b21b655be2e5303c03
Kate465 <kategokeborate@gmail.com>
2025-03-02 14:33:30 +0000

    added search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <p>This project was coded by Katego and is open sourced on github</p>
+      <Weather />
+      <p>
+        This project was coded by Katego and is{" "}
+        <a href="https://github.com/Kate465/Weather-React-App" rel="noopener">
+          open sourced on github{" "}
+        </a>
+      </p>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-1">
+            <input type="submit" value="search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="mostly cloudy"
+          />
+          6℃
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation:15%</li>
+            <li>Humidity:72%</li>
+            <li>Wind:13km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
